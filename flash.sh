3fa73ca1d242cae4874d152e2bc7ae1f75960db6
k-off <pacovali@student.42berlin.de>
2024-12-28 10:41:02 +0100

    update path

--- flash.sh
+++ flash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pico=$(ls /dev | grep ACM)
 
-gdb-multiarch -ex "target extended-remote /dev/${pico}"  -ex 'load' -ex 'detach' -ex 'quit' ".pio/build/Demo-Sound/firmware.elf"
+gdb-multiarch -ex "target extended-remote /dev/${pico}"  -ex 'load' -ex 'detach' -ex 'quit' ".pio/build/Demo-38C3/firmware.elf"
